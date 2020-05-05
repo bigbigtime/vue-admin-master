8baf98978a4e0a5b4c368170852440e3dd607086
bigbigtime <409019683@qq.com>
2020-05-05 10:41:30 +0800

    lesson3.4-home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>这里是测试文字</h1>
+    <ul>
+      <li>无序列表LI</li>
+      <li>无序列表LI</li>
+    </ul>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
