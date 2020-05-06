55621c02384f2201f97ab631507258478f5ee0b1
webjshtml <410293095@qq.com>
2020-05-06 18:53:33 +0800

    重定向

---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>登录页面</div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  super(props, { root }){
+      console.log(root)
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
