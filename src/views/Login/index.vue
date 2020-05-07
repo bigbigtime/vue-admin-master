b2d15987533882f44988411d03e5d3f7533bc3c5
webjshtml <410293095@qq.com>
2020-05-07 17:26:49 +0800

    scoped

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>登录页面</div>
+<div id="login">
+<div class="form-wrap">
+  <ul class="menu-tab">
+    <li class="current">登录</li>
+    <li>注册</li>
+  </ul>
+</div>
+</div>
 </template>
 
 <script>
@@ -11,4 +18,26 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+.form-wrap {
+  width: 300px;
+  padding-top: 100px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    padding: 10px 24px;
+    margin: 0 10px;
+    color: #fff;
+    font-size: 14px;
+    border-radius: 5px;
+    cursor: pointer;
+    &.current { background-color: rgba(0, 0, 0, .1);}
+  }
+}
 </style>
