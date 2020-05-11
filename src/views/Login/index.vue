69844fed326bcabf9a6b745d8de552beed3c0a71
webjshtml <410293095@qq.com>
2020-05-11 16:24:20 +0800

    login

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -40,7 +40,7 @@
 import { reactive, ref } from "@vue/composition-api";
 export default {
   name: "Login",
-  setup(props, { root }){
+  setup(props, { root, refs }){
     const menu_switch_item = reactive([
       { type: "login", label: "登录" },
       { type: "register", label: "注册" }
@@ -52,17 +52,51 @@ export default {
       passwords: "",
       code: ""
     })
+    /**
+     * 自定义检验规则
+     */
+    // 检验邮箱
+    const validate_name_rules = (rule, value, callback) => {
+      let regEmail = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+      if (value === '') {
+        callback(new Error("请输入邮箱"));
+      } else if(!regEmail.test(value)) {
+        callback(new Error("邮箱格式不正确"));
+      } else {
+        callback();
+      }
+    };
+    // 检验密码
+    const validate_password_rules = (rule, value, callback) => {
+      let regPassword = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
+      if (value === '') {
+        callback(new Error("请输入密码"));
+      } else if(!regPassword.test(value)) {
+        callback(new Error("请输入 >=6 并且 <=20 位的密码，包含数字、字母"));
+      } else {
+        callback();
+      }
+    };
+    // 检验验证码
+    const validate_code_rules = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error("请输入验证码"));
+      } else if(value.length !== 6) {
+        callback(new Error("请输入长度为6位数的验证码"));
+      } else {
+        callback();
+      }
+    };
     // 检验规则
     const form_rules = reactive({
-      name: [
-        { required: true, message: '请输入活动名称', trigger: 'blur' },
-        { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+      name: [ 
+        { validator: validate_name_rules, trigger: 'blur' }
       ],
       password: [
-        { required: true, message: '请输入密码', trigger: 'blur' }
+        { validator: validate_password_rules, trigger: 'blur' }
       ],
       code: [
-        { required: true, message: '请输入验证码', trigger: 'blur' }
+        { validator: validate_code_rules, trigger: 'blur' }
       ]
     });
     // 切换样式方法
