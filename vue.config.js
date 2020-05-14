41c9d82e519a6573e07febb1ee867d29652e7ef4
bigbigtime <409019683@qq.com>
2020-05-14 08:12:00 +0800

    login

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,23 @@ module.exports = {
    */
   pwa: {},
   // webpack-dev-server 相关配置
-  devServer: {},
+  devServer: {
+    open: false, // 编译完成是否打开网页
+    host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
+    port: 8080, // 访问端口
+    https: false, // 编译失败时刷新页面
+    hot: true, // 开启热加载
+    hotOnly: false,
+    proxy: {
+      "/api": {
+          target: "http://www.web-jshtml.cn/api/vue3", //API服务器的地址
+          changeOrigin: true,
+          pathRewrite: {
+              "^/api": ''
+          }
+      }
+    }
+  },
   /**
    * 第三方插件配置
    */
