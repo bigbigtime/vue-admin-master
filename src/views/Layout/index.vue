0b50d608d1e3c960b4f55f8b9cc18a6c4dc40846
bigbigtime <409019683@qq.com>
2020-05-21 23:08:32 +0800

    layout

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,8 @@
 <template>
     <el-container id="layout-container">
-        <el-aside id="layout-aside">侧栏菜单</el-aside>
+        <el-aside id="layout-aside" width="250px">侧栏菜单</el-aside>
         <el-container>
-            <el-header id="layout-header">头部信息</el-header>
+            <el-header id="layout-header" height="75px">头部信息</el-header>
             <el-main id="layout-main">数据内容</el-main>
         </el-container>
     </el-container>
@@ -14,10 +14,7 @@ export default {
 </script>
 <style lang="scss" scpoed>
 #layout-container { height: 100vh; }
-#layout-aside {
-    width: 200px;
-    background-color: #77acbc;
-}
+#layout-aside { background-color: #77acbc; }
 #layout-header { background-color: #77bc99; }
 #layout-main { background-color: #bc77a1; }
 </style>
