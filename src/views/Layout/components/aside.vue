ebe77ddb61b2156c47775c864f92e4287eeeb549
webjshtml <410293095@qq.com>
2020-05-22 15:16:51 +0800

    aside

--- src/views/Layout/components/aside.vue
+++ src/views/Layout/components/aside.vue
@@ -1,12 +1,12 @@
 <template>
   <el-menu default-active="2" class="el-menu-vertical-demo" background-color="#344a5f" text-color="#fff" active-text-color="#ffd04b">
-<el-submenu v-for="item in router" :key="item.id" index="1">
-  <template slot="title">
-    <i class="el-icon-location"></i>
-    <span>{{ item.meta ? item.meta.title : "" }}</span>
-  </template>
-  <el-menu-item index="1-4-1">选项1</el-menu-item>
-</el-submenu>
+  <el-submenu v-for="(item, index) in router" :key="item.id" :index="index + ''">
+    <template slot="title">
+      <i class="el-icon-location"></i>
+      <span>{{ item.meta ? item.meta.title : "" }}</span>
+    </template>
+    <el-menu-item index="1-4-1">选项1</el-menu-item>
+  </el-submenu>
   </el-menu>
 </template>
 
