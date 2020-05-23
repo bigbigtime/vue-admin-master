6c4c0f16c826edbffc85a6a6256533c03b998cb8
bigbigtime <409019683@qq.com>
2020-05-23 16:24:48 +0800

    svgicon

--- src/icon/svgIcon.vue
+++ src/icon/svgIcon.vue
@@ -1,44 +1,14 @@
 <template>
-    <svg class="svg-icon" :class="svgClass" aria-hidden="true">
+    <svg class="svg-icon" :class="className" aria-hidden="true">
         <use :xlink:href="iconName"></use>
     </svg>
 </template>
 <script>
 import { ref, computed, onMounted } from '@vue/composition-api';
 export default {
-    name: "svgIcon",
-    // props: ['iconClass', 'className'],
-    props: {
-        // 分页
-        iconClass: {
-            type: String,  // 定义接收的值类型
-            default: ''
-        },
-        className : {
-            type: String,
-            default: ''
-        }
-    },
+    name: "SvgIcon",
     setup(props){
-        const msg = ref("手把手撸码的说法的前端");
-        const number = ref(1);
-
-        // 计算属性，得出最终结果，并返回
-        const iconName = computed(() => `#icon-${props.iconClass}`)
-
-        const svgClass = computed(() => {
-            if(props.className) {
-                return `svg-icon ${props.className}`
-            }else{
-                return `svg-icon`
-            }
-        })
-
-        return {
-            msg,
-            iconName,
-            svgClass
-        }
+        return {}
     }
 }
 </script>
