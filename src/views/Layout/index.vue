c909a0ddc4cbd01a74c5316de2f2a77bf4dd8307
webjshtml <410293095@qq.com>
2020-05-25 17:32:22 +0800

    transition

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,6 @@
 <template>
     <el-container id="layout-container">
-        <el-aside id="layout-aside" width="250px"><LayoutAside/></el-aside>
+    <el-aside id="layout-aside" :width="collapse === true ? '60px' : '250px'"><LayoutAside/></el-aside>
         <el-container>
             <el-header id="layout-header" height="75px"><LayoutHeader/></el-header>
             <el-main id="layout-main"><LayoutMain/></el-main>
@@ -8,17 +8,27 @@
     </el-container>
 </template>
 <script>
+// 组件
 import LayoutAside from "./components/aside";
 import LayoutHeader from "./components/header";
 import LayoutMain from "./components/main";
+// 3.0 API
+import { computed } from "@vue/composition-api";
 export default {
     name: "Layout",
-    components: { LayoutAside, LayoutHeader, LayoutMain }
+    components: { LayoutAside, LayoutHeader, LayoutMain },
+    setup(props, { root }){
+        const collapse = computed(() => root.$store.state.app.collapse);
+        return { collapse }
+    }
 }
 </script>
 <style lang="scss" scpoed>
 #layout-container { height: 100vh; }
-#layout-aside { background-color: #344a5f; }
+#layout-aside {
+    background-color: #344a5f;
+    @include webkit(transition, all .3s ease 0s);
+}
 #layout-header {
     position: relative;
     z-index: 10;
