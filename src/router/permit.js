153b5b65fd0dc2cbb2010eaad8f2fd8c0211657b
webjshtml <410293095@qq.com>
2020-05-28 11:54:05 +0800

    token

--- src/router/permit.js
+++ src/router/permit.js
@@ -1,7 +1,12 @@
 import router from "./index";
+// cookie
+import { getToken } from "@/utils/cookies";
 // 全局前置守卫
 router.beforeEach((to, from, next) => {
-    next();
+    // 获取 token 是否存在
+    if(getToken()){
+        next();
+    }
 })
 // 全局后置守卫
 router.afterEach((to, from) => {
