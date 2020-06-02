c2ee24895765d7bb8fb08b340acb734bf302b7c3
bigbigtime <409019683@qq.com>
2020-06-02 23:54:44 +0800

    news

--- src/views/News/index.vue
+++ src/views/News/index.vue
@@ -2,10 +2,17 @@
   <div class="news-list">
     <el-row>
       <el-col :span="18">
-        <div class="filter-wrap">
-          <div class="inline">类型</div>
-          <div class="inline">关键字</div>
+      <div class="filter-wrap">
+        <div class="inline">
+          <div class="filter-item mr-26">
+            <label>类型：</label>
+            <el-select v-model="data.category" style="width: 160px;">
+              <el-option v-for="item in data.category_opacity" :key="item.value" :value="item.value" :label="item.label"></el-option>
+            </el-select>
+          </div>
         </div>
+        <div class="inline">关键字</div>
+      </div>
       </el-col>
       <el-col :span="6">
         <div class="text-right"><el-button type="danger">新增</el-button></div>
@@ -21,24 +28,28 @@ export default {
   components: {},
   props: {},
   setup(props, { root }){
-    // 监听
-    // watch(xxx, () => {})
-    // 生命周期
-    // onBeforeMount(() => {})
-    // onMounted(() => {})
-    // onBeforeUpdate(() => {})
-    // onUpdated(() => {})
-    // onBeforeUnmount(() => {})
-    // onUnmounted(() => {})
-    // onErrorCaptured(() => {})
-    // onRenderTracked(() => {})
-    // onRenderTriggered(() => {})
-    return {}
+    const data = reactive({
+      category: 0,
+      category_opacity: [
+        { label: "人工智能", value: 0 },
+        { label: "技术", value: 1 }
+      ]
+    });
+    return { data }
   }
 }
 </script>
 <style lang="scss" scoped>
 .filter-wrap {
   .inline { display: inline-block; }
+}
+.filter-item {
+  height: 40px;
+  label {
+    float: left;
+    padding-right: 10px;
+    line-height: 40px;
+    font-size: 14px;
+  }
 }
 </style>
