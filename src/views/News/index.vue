12c7597f4ce05757c542d02951f476f87ef71a80
bigbigtime <409019683@qq.com>
2020-06-02 23:53:01 +0800

    new

--- src/views/News/index.vue
+++ src/views/News/index.vue
@@ -3,20 +3,8 @@
     <el-row>
       <el-col :span="18">
         <div class="filter-wrap">
-          <div class="inline">
-            <div class="filter-item mr-26">
-              <label>类型：</label>
-              <el-select style="width: 160px;"></el-select>
-            </div>
-          </div>
-          <div class="inline">
-            <div class="filter-item">
-              <label>关键字：</label>
-              <el-select style="width: 100px;" class="mr-15"></el-select>
-              <el-input placeholder="输入关键字按钮enter搜索" style="width: 220px;" class="mr-15"></el-input>
-              <el-button type="danger">搜索</el-button>
-            </div>
-          </div>
+          <div class="inline">类型</div>
+          <div class="inline">关键字</div>
         </div>
       </el-col>
       <el-col :span="6">
@@ -52,14 +40,5 @@ export default {
 <style lang="scss" scoped>
 .filter-wrap {
   .inline { display: inline-block; }
-}
-.filter-item {
-  height: 40px;
-  label {
-    float: left;
-    padding-right: 10px;
-    line-height: 40px;
-    font-size: 14px;
-  }
 }
 </style>
