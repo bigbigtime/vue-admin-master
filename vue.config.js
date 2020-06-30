82091afdf2a1abda649a95be717215d8d8c178e3
yanghaimin <yanghaimin@rockminer.com>
2020-06-30 15:28:20 +0800

    跨域

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,23 @@ module.exports = {
    */
   pwa: {},
   // webpack-dev-server 相关配置
-  devServer: {},
+  devServer: {
+    open: false, // 编译完成是否打开网页
+    host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
+    port: 8080, // 访问端口
+    https: false, // 编译失败时刷新页面
+    hot: true, // 开启热加载
+    hotOnly: false,
+    proxy: {
+      "/api": {  // ①
+        target: "http://www.web-jshtml.cn/api/vue3",  //API服务器的地址
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""  // ② // 匹配开头为 /api的字符串，并替换成空字符串
+        }
+      }
+    }
+  },
   /**
    * 第三方插件配置
    */
