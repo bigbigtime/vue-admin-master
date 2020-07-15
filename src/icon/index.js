1b94c53dc335e8203b811bcec2187d6e04fe2d28
yanghaimin <yanghaimin@rockminer.com>
2020-07-15 09:32:50 +0800

    require.context

--- src/icon/index.js
+++ src/icon/index.js
@@ -1,2 +1,7 @@
 import Vue from "vue";
 Vue.component("svg-icon", SvuIcon);
+const req = require.context('./svg', false, /\.svg$/);  // 获取当前svg目录所有为.svg结尾的文件
+const requireAll = (requireContext) => {
+    return requireContext.keys().map(requireContext);  // 解析获取的.svg文件的文件名称，并返回
+};
+requireAll(req); // 调用方法
