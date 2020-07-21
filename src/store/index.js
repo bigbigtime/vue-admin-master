eb17e3100f8235fb1b3176b693c9ac7811c42c0d
yanghaimin <yanghaimin@rockminer.com>
2020-07-21 14:44:35 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
 Vue.use(Vuex);
-
-import app from "./modules/app";
 export default new Vuex.Store({
-  modules: {
-    app
-  }
+  state: {},
+  getters: {},
+  actions: {},
+  mutations: {}
 });
