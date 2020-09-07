4f96e4d08556882b3367e62b1d63c62da007db7e
bigbigtime <409019683@qq.com>
2020-09-08 07:50:30 +0800

    BasisTable

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -1,9 +1,7 @@
 <template>
     <div>
         <el-table :data="data.tableData" border style="width: 100%">
-            <el-table-column prop="date" label="日期"></el-table-column>
-            <el-table-column prop="name" label="姓名"></el-table-column>
-            <el-table-column prop="address" label="地址"></el-table-column>
+            <el-table-column v-for="item in config.thead" :key="item.prop" :prop="item.prop" :label="item.label"></el-table-column>
         </el-table>
     </div>
 </template>
@@ -13,12 +11,21 @@ export default {
     name: "BasisTable",
     props: {},
     setup(props, context){
-        const data = reactive({
-            tableData: [
-                { date: '2016-05-02', name: '王小虎', address: '上海市普陀区金沙江路 1518 弄' }
+        const config = reactive({
+            thead: [
+                { label: "日期", prop: "date" },
+                { label: "姓名", prop: "name" },
+                { label: "地址", prop: "address" },
+                { label: "年龄", prop: "age" },
+                { label: "性别", prop: "gender" },
             ]
         })
-        return { data };
+        const data = reactive({
+            tableData: [
+                { date: '2016-05-02', name: '王小虎', address: '上海市普陀区金沙江路 1518 弄', age: 18, gender: "男" }
+            ]
+        })
+        return { data, config }
     }
 }
 </script>
