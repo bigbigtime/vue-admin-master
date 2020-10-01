d033f78417d183007e0d4595a9298a88ed84c31c
bigbigtime <409019683@qq.com>
2020-10-01 10:44:04 +0800

    BasisForm

--- src/components/form/index.vue
+++ src/components/form/index.vue
@@ -1,8 +1,32 @@
 <template>
     <el-form>
-        <el-form-item v-for="item in formItem" :key="item.prop" :label="item.label" :prop="item.prop">
-            表单元素类型
-        </el-form-item>
+        <template v-for="item in formItem">
+            <!-- select -->
+            <el-form-item v-if="item.type === 'categoryInfo'" :key="item.prop" :label="item.label" :prop="item.prop">
+                
+            </el-form-item>
+            <!-- input -->
+            <el-form-item v-if="item.type === 'input'" :key="item.prop" :label="item.label" :prop="item.prop">
+                
+            </el-form-item>
+            <!-- upload -->
+            <el-form-item v-if="item.type === 'upload'" :key="item.prop" :label="item.label" :prop="item.prop">
+                
+            </el-form-item>
+            <!-- date -->
+            <el-form-item v-if="item.type === 'date'" :key="item.prop" :label="item.label" :prop="item.prop">
+                
+            </el-form-item>
+            <!-- date -->
+            <el-form-item v-if="item.type === 'radio'" :key="item.prop" :label="item.label" :prop="item.prop">
+                
+            </el-form-item>
+            <!-- date -->
+            <el-form-item v-if="item.type === 'wangeditor'" :key="item.prop" :label="item.label" :prop="item.prop">
+                
+            </el-form-item>
+        </template>
+        
     </el-form>
 </template>
 <script>
