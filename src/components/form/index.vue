a2706825a4f5f977b5ed144920b4fd8d617a6f1c
yanghaimin <yanghaimin@rockminer.com>
2020-10-10 17:46:55 +0800

    wangEditor

--- src/components/form/index.vue
+++ src/components/form/index.vue
@@ -1,41 +1,42 @@
 <template>
-<el-form>
-    <template v-for="item in formItem">
-        <!-- select -->
-        <el-form-item v-if="item.type === 'cascader'" :key="item.prop" :label="item.label" :prop="item.prop">
-            <CascaderVue :url="item.url" />
-        </el-form-item>
-        <!-- input -->
-        <el-form-item v-if="item.type === 'input'" :key="item.prop" :label="item.label" :prop="item.prop">
-            
-        </el-form-item>
-        <!-- upload -->
-        <el-form-item v-if="item.type === 'upload'" :key="item.prop" :label="item.label" :prop="item.prop">
-            <UploadVue :requestData="item.requestData" :url="item.url" />
-        </el-form-item>
-        <!-- date -->
-        <el-form-item v-if="item.type === 'date'" :key="item.prop" :label="item.label" :prop="item.prop">
-            
-        </el-form-item>
-        <!-- date -->
-        <el-form-item v-if="item.type === 'radio'" :key="item.prop" :label="item.label" :prop="item.prop">
-            
-        </el-form-item>
-        <!-- date -->
-        <el-form-item v-if="item.type === 'wangeditor'" :key="item.prop" :label="item.label" :prop="item.prop">
-            <el-button @click="get">fff</el-button>
-        </el-form-item>
-    </template>
-</el-form>
+    <el-form>
+        <template v-for="item in formItem">
+            <!-- select -->
+            <el-form-item v-if="item.type === 'cascader'" :key="item.prop" :label="item.label" :prop="item.prop">
+                <CascaderVue :url="item.url" />
+            </el-form-item>
+            <!-- input -->
+            <el-form-item v-if="item.type === 'input'" :key="item.prop" :label="item.label" :prop="item.prop">
+                
+            </el-form-item>
+            <!-- upload -->
+            <el-form-item v-if="item.type === 'upload'" :key="item.prop" :label="item.label" :prop="item.prop">
+                <UploadVue :requestData="item.requestData" :url="item.url" />
+            </el-form-item>
+            <!-- date -->
+            <el-form-item v-if="item.type === 'date'" :key="item.prop" :label="item.label" :prop="item.prop">
+                
+            </el-form-item>
+            <!-- date -->
+            <el-form-item v-if="item.type === 'radio'" :key="item.prop" :label="item.label" :prop="item.prop">
+                
+            </el-form-item>
+            <!-- date -->
+            <el-form-item v-if="item.type === 'wangeditor'" :key="item.prop" :label="item.label" :prop="item.prop">
+                <WangEditor />
+            </el-form-item>
+        </template>
+    </el-form>
 </template>
 <script>
 // components
 import CascaderVue from "@c/cascader";
 import UploadVue from "@c/upload";
+import WangEditor from "@c/wangEditor";
 import { ref } from "@vue/composition-api";
 export default {
     name: 'BasisForm',
-    components: { CascaderVue, UploadVue },
+    components: { CascaderVue, UploadVue, WangEditor },
     props: {
         formItem: {
             type: Array,
